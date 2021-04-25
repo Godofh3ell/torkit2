a71a129160f5c726b005e22d9c4d536651900f93
Godofh3ell <66544811+Godofh3ell@users.noreply.github.com>
2021-04-25 18:56:42 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,17 +5,17 @@ except:
         # Set true if its VPS
         IS_VPS = False
         
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "97bebcbb692546bce29934291c5c2998"
+        API_ID = 2068926
+        BOT_TOKEN = "1415747350:AAGgVUBfB8-oDfbY7T7S62QwUS_XsoeIvo4"
+        BASE_URL_OF_BOT = "https://megatunemedoet.herokuapp.com"
 
         # Edit the server port if you want to keep it default though.
         SERVPORT = 80
 
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
-        OWNER_ID = 0
+        ALD_USR = [-1001209420449]
+        OWNER_ID = 810796147
         
         # Google Drive Index Link should include the base dir also See readme for more info
         GD_INDEX_URL = False
